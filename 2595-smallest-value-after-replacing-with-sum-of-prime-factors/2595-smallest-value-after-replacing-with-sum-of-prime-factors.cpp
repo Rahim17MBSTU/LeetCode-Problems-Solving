48664b004676ca9e3e6b436dc5cc962f8b7380c2
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-07-06 15:55:24 +0600

    Time: 3 ms (57.8%), Space: 7.2 MB (57.64%) - LeetHub

--- 2595-smallest-value-after-replacing-with-sum-of-prime-factors/2595-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
+++ 2595-smallest-value-after-replacing-with-sum-of-prime-factors/2595-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -2,36 +2,24 @@ class Solution {
 public:
 
     int smallestValue(int n) {
-        if(n == 4)return 4;
-        vector<bool>b (1e5+10, 0 );
-        vector<int>prime;
-        for(int i = 2;i*i<=1e5;i++){
-            if(b[i] == 0){
-                for(int j = i * i ;j <= 1e5;j+=i){
-                    b[j] = 1;
-                }
-            }
-        }
-        for(int i=2;i<=1e5;i++){
-            if(b[i] == 0) prime.push_back(i);
-        }
-        if(b[n] == 0){
-            return n;
-        }
         while(1){
-            int new_n = 0;
-            for(int i = 0 ;i < prime.size(); i++){
-                if(n % prime[i] == 0){
-                    while(n%prime[i] == 0 && n>1){
-                        new_n += prime[i];
-                        n/=prime[i];
+            int new_n = 0, temp = n;
+
+            for(int i=2;i*i<=n;i++){
+                if(n%i == 0){
+                    while(n%i == 0){
+                        n/=i;
+                        new_n += i;
                     }
                 }
-                if(prime[i]>n)break;
-                
             }
-            n = new_n;
-            if(b[n] == 0)return n;
+            if(n>1)new_n += n;
+            
+            if(new_n == temp){
+                return temp;
+            }
+            n = new_n; 
         }
+        
     }
 };
