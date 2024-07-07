86d15cdc5c5726f8e96f3d0190eaae0b29ea1e39
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-07-07 15:21:05 +0600

    Time: 481 ms (94.54%), Space: 208.5 MB (91.94%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     static const int N = 2e5+10;
-
     int parent [N],size[N];
     void make(int v){
         parent[v] = v;
-        size[v] = 1;
+        //size[v] = 1;
     }
     int find(int u){
         if(u == parent[u]){
@@ -17,7 +16,7 @@ public:
         int a1 = find(a);
         int b1 = find(b);
         if(a1 != b1){
-            if(size[a1] < size[b1]) swap(a1,b1);
+            //if(size[a1] < size[b1]) swap(a1,b1);
            parent[b1] = a1;
            
         }
@@ -25,19 +24,18 @@ public:
     
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         
-        int n1 = edges.size();
+        int sz = edges.size();
         for(int i=0;i<n;i++) {
             make(i);
         }
-        for(int i=0;i<n1;i++){
+        for(int i=0;i<sz;i++){
             int u = edges[i][0];
             int v = edges[i][1];
             Union(u,v);
             
         }
-        if(find(source) != find(destination)){
-            return false;
-        }
-        return true;
+        return find(source) == find(destination) ;
+        
+        
     }
 };
