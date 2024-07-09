a46346962c9a37e87a295072dab931d52180c811
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-07-10 01:02:19 +0600

    Time: 30 ms (5.17%), Space: 16.1 MB (97.13%) - LeetHub

--- 1104-coloring-a-border/1104-coloring-a-border.cpp
+++ 1104-coloring-a-border/1104-coloring-a-border.cpp
@@ -1,56 +1,43 @@
 class Solution {
 public:
-    int dx[8] = {-1, 1, 0, 0};
-    int dy[8] = {0, 0, -1, 1};
+    int dx[4] = {-1,0,0,1};
+    int dy[4] = { 0,-1,1,0};
     bool vis[55][55];
-    void dfs(int row,int col , int color, vector<vector<int>>& grid, int n,int m,int init_col,vector<vector<int>>& grid1){
-        
-        if(row <0 || col <0 || row>= n || col >= m || grid[row][col]!= init_col || vis[row][col] == 1)return;
-        bool ok = 0;
-        if(row-1 >=0 && grid[row-1][col] != init_col){
-            ok = 1;
-            
-            grid1[row][col] = color;
-        }else if(row -1 < 0 && ok == 0 ){
-            ok = 1;
-            
-            grid1[row][col] = color;
-        }
-        if(row+1 <n && grid[row+1][col] != init_col && ok == 0){
-            ok = 1;
-            grid1[row][col] = color;
-        }else if(row+1 ==n  && ok == 0){
-            ok = 1;
-            grid1[row][col] = color;
-        }if(col-1 >=0 && grid[row][col-1] != init_col && ok == 0){
-            ok = 1;
-            grid1[row][col] = color;
-        }else if(col-1 <0  && ok == 0){
-            ok = 1;
-            grid1[row][col] = color;
-        }if(col+1 <m && grid[row][col + 1] != init_col && ok ==0){
-            ok = 1;
-            grid1[row][col] = color;
-        }else if(col+1 ==m && ok ==0){
-            ok = 1;
-            grid1[row][col] = color;
-        }
-       
-        
+    void dfs(int row,int col, vector<vector<int>>&grid,int color,int rep_col){
+        grid[row][col] = -color;
         vis[row][col] = 1;
+        int cnt = 0;
         for(int i=0;i<4;i++){
-            int x = dx[i] + row;
-            int y = dy[i] + col;
-            dfs(x,y,color,grid,n,m,init_col,grid1);
+            int x = row + dx[i];
+            int y = col + dy[i];
+            cout<<x<<" "<<y<<endl;
+            if(x < 0 || y < 0 || x >=grid.size() || y >= grid[0].size() || abs(grid[x][y]) != color ){
+                cout<<"aise1 \n";
+                continue;
+            }
+            cnt++;
+            if(!vis[x][y] && grid[x][y] == color){
+                cout<<"aise2 = "<<cnt<<endl;;
+                dfs(x,y,grid,color,rep_col);
+            }
+            cout<<"cnt= "<<cnt<<endl;
             
         }
+        if(cnt == 4){
+                grid[row][col] = color;
+                cout<<"row ="<<row<<" col= "<<col<<endl;
+         }
+
+
     }
     vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int init_col = grid[row][col];
-        vector<vector<int>> grid1 = grid;
-        dfs(row,col,color,grid,n,m,init_col,grid1);
-        return grid1;
+        
+        dfs(row,col,grid,grid[row][col],color);
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j] < 0) grid[i][j] = color;   
+            }
+        }
+        return grid;
     }
 };
