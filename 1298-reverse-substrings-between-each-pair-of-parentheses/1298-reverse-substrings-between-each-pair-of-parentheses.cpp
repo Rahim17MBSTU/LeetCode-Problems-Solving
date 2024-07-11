c47815d348d9dabc3ef16929b02ac27fb113a6d2
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-07-11 16:36:37 +0600

    Time: 2 ms (53.02%), Space: 7.9 MB (49.11%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,36 +1,29 @@
-#include <iostream>
-#include <stack>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     string reverseParentheses(string s) {
-        stack<string> st;
-        string current = "";
-        
-        for (char ch : s) {
-            if (ch == '(') {
-                st.push(current);
-                current = "";
-            } else if (ch == ')') {
-                reverse(current.begin(), current.end());
-                //cout<<"current= "<<current<<endl;
-                //cout<<"st-size= "<<st.size()<<endl;
-                if (!st.empty()) {
-                    //cout<<"cholse\n";
-                    //cout<<"st= "<<st.top()<<endl;
-                    current = st.top() + current;
-                    cout<<current<<endl;
+        stack<char>st;
+        for(auto u:s){
+            if(u == ')'){
+                string temp = "";
+                while(1){
+                    char c = st.top();
                     st.pop();
+                    if(c == '(')break;
+                    temp+=c;
+                }cout<<temp<<endl;
+                
+                for(auto v:temp){
+                    st.push(v);
                 }
-            } else {
-                current += ch;
+            }else{
+                st.push(u);
             }
         }
-        
-        return current;
+        s = "";
+        while(!st.empty()){
+            s+=st.top();
+            st.pop();
+        }reverse(s.begin(),s.end());
+        return s;
     }
 };
-
-
