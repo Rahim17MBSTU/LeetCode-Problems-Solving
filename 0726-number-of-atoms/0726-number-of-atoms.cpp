da0dd865b5d97c19ca63d596c1f0b15a5ece28d6
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-07-14 15:40:21 +0600

    Time: 4 ms (40.97%), Space: 9.3 MB (56.72%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.cpp
+++ 0726-number-of-atoms/0726-number-of-atoms.cpp
@@ -3,7 +3,32 @@ public:
     string countOfAtoms(string formula) {
         stack<pair<string,int>>st;
         int n = formula.size();
+        stack<int>st1;
+        vector<int>v(n);
+        int factor = 1;
+        for(int i = n-1;i>=0;i--){
+            if(formula[i] ==')'){
+                int j = i+1;
+                string digit="";
+                for(;j<n;j++){
+                    if(isdigit(formula[j])) digit+=formula[j];
+                    else break;
+                }
+                if(digit =="")digit ="1";
+                int x = stoi(digit);
+                factor *= x;
+                st1.push(x);
+
+            }else if(formula[i] == '(') {
+                int digit = st1.top();
+                st1.pop();
+                factor/= digit;
+            }
+            v[i] = factor;
+        }
+        map<string,int>mp;
         for(int i=0;i<n;i++){
+            int factor = v[i];
             string ss = "";
             if(isupper(formula[i])){
                int j = i+1;
@@ -14,12 +39,11 @@ public:
                  }
                  else 
                  {
-                    //j--;
                     break;
                  }
                }
                int k = j;
-               //cout<<"k ="<<k<<endl;
+               
                string digit = "";
                for(;k<n;k++){
                 if(isdigit(formula[k])){
@@ -31,50 +55,13 @@ public:
               
                int x = 1;
                if(digit.size()>=1) x = stoi(digit);
+               mp[ss] += (x*factor);
               
-               st.push({ss,x});
                i = k-1;
               
-            }else if(formula[i] =='('){
-                ss+=formula[i];
-                st.push({ss,0});
-            }else if(formula[i] == ')'){
-                
-                int j = i + 1;
-                string digit = "";
-                for(;j<n;j++){
-                    if(isdigit(formula[j]))
-                    digit+=formula[j];
-                    else{
-                        break;
-                    }
-
-                }
-                i = j-1;
-                int value = 1;
-                if(digit.size()>=1)value *= stoi(digit);
-                
-                vector<pair<string,int>>vp;
-                while(!st.empty()){
-                    string d = st.top().first;
-                    int k = st.top().second;
-                    st.pop();
-                    if(d == "(" ) break;
-                    else{
-                        k *= value;
-                        vp.push_back({d,k});
-                    }
-                    
-                }
-                while(!vp.empty()){
-                    string x = vp.back().first;
-                    int y = vp.back().second;
-                    vp.pop_back();
-                    st.push({x,y});
-                }
             }
-        }
-        map<string,int>mp;
+         }
+        
         while(!st.empty()){
             string ss = st.top().first;
             int x = st.top().second;
