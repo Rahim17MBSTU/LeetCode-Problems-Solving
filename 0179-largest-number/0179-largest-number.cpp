33fd5afe598c0e1322d186c1f2222739dc873c18
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-09-30 00:29:10 +0600

    Time: 7 ms (58.16%), Space: 18.2 MB (12.04%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -6,11 +6,48 @@ using namespace std;
 
 class Solution {
 public:
-    
+    // static bool comp(string p1, string p2)
+    // {
+        
+    //     int i = 0,j = 0;
+    //     int x = p1.size();
+    //     int y = p2.size();
+    //         cout<<"aise "<<p1 <<" "<<p2<<endl;
+    //         for(;i<(x+y) && j<(x+y);j++,i++){
+    //             //cout<<"cholse = "<<i<< " "<<j<<endl;
+    //                             if(i == p1.size()){
+    //                 i = 0;
+    //             }
+    //             if(j == p2.size()){
+    //                 j = 0;
+    //             }cout<<p1[i]<<" "<<p2[j]<<" "<<i<<" "<<j<<endl;
+
+    //             if(p1[i] == p2[j])continue;
+    //             else if(p1[i] > p2[j]){
+    //                 cout<<p1 <<" > "<<p2<<endl;
+    //                 return p1 > p2;
+    //             }
+                    
+    //             else if(p1[i] < p2[j]){
+    //                 cout << p1 << " < "<<p2<<endl;
+    //                 return p2 > p1;
+    //             }
+                   
+    //         }
+    //         //cout<<"kkkk\n";
+    //         return p1>p2;
+        
+        
+    // }
     static bool comp(string p1, string p2)
     {
-        string first = p1 + p2;
-        string second = p2 + p1;
+    int x = p1.size();
+    int y = p2.size();
+
+    
+    string first = p1 + p2;
+    string second = p2 + p1;
+
         return first > second;
     }
     string largestNumber(vector<int>& nums) {
@@ -22,11 +59,31 @@ public:
             string s = to_string(u);
             v.push_back(s);
         }
-        sort(v.begin(),v.end(),comp);
-        for(auto u:v){
-            result += u;
+        sort(v.rbegin(),v.rend());
+        // for(auto u:v){
+        //     cout << u <<endl;
+        // }
+        // cout<<endl;
+        for(int i=9;i>=0;i--)
+        {
+            vector<string>vp;
+            for(int j = 0 ;j < n ;j++){
+
+                string s = to_string(nums[j]);
+                if(s[0]-'0' == i)
+                {
+                    vp.push_back(s);
+                }
+            }
+            if(!vp.empty()){
+                sort(vp.begin(),vp.end(),comp);
+                for(auto u:vp){
+                    cout<<u<<endl;
+                    result += u;
+                }
+                
+            }
         }
-        
         bool ok = 0;
         for(int i=0;i<result.size()-1;i++){
             if(result[i] == result[i+1] && result[i] == '0'){
