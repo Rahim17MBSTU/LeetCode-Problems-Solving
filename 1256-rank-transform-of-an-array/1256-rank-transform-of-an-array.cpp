74fcbde3ba12e96ba33b0ce1fdae9bd34327fba7
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-02 22:08:39 +0600

    Time: 103 ms (26.95%), Space: 51.5 MB (8.3%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         int n = arr.size(),cnt = 0;
-        map<int,int>mp;set<int>s(arr.begin(),arr.end());
+        unordered_map<int,int>mp;set<int>s(arr.begin(),arr.end());
         for(auto u:s){
             mp[u] = ++cnt;
         }vector<int>res;
