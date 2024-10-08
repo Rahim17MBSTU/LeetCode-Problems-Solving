c7426b1d4a50800ac009343856b89a084c07cd70
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-08 11:47:55 +0600

    Time: 2577 ms (5%), Space: 497.1 MB (5%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        
         int n = nums.size();
-        map<pair<int,pair<int,int>>,int>mp;
-        vector<vector<int>> res;
+        
+        set<vector<int>>vp;
+        
+        
+        
         sort(nums.begin(),nums.end());
+        
         for(int i=0;i<n;i++){
             int target = nums[i] * (-1);
-            int j = 0 , k = n-1;
+            int j = 1 , k = n-1;
             bool ok = 0;
             while(j<=k){
                 int sum = nums[j] + nums[k];
                 if(i != j && i!=k && j!=k && sum == target){
                     vector<int>v{nums[i],nums[j],nums[k]};
                     sort(v.begin(),v.end());
-                    int first = v[0], second= v[1] , third = v[2];
-                    if(mp[{first,{second, third} }] == 0){
-                        //cout<<"aise = "<<i<<" "<<j<< " "<<k<<endl;
-                        //vector<int>vv {nums[v[0]],nums[v[1]],nums[v[2]]};
-                        res.push_back(v);
-                        mp[{first,{second, third} }]++;
-                        //ok  = 1 ;
-                    }
+                    vp.insert(v);
                     j++;
                     k--;
-                    //if(ok == 1)
-                    //break;
                 }
                 else if(sum > target ){
                     k--;
@@ -35,6 +31,7 @@ public:
             }
             
         }
+        vector<vector<int>> res(vp.begin(),vp.end());
         return res;
     }
 };
