b63590c6a7de8fc901fa516de0b7a8cf322c8dc9
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-08 16:38:14 +0600

    Time: 23 ms (77.62%), Space: 16.2 MB (72.39%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,30 +4,30 @@ public:
         
         sort(nums.begin(),nums.end());
 
-        map<pair<int,pair<int,pair<int,int>>>,int>mp;
+        //map<pair<int,pair<int,pair<int,int>>>,int>mp;
         
         vector<vector<int>>vp;
 
         int n = nums.size();
         
         for(int i = 0 ;i <n-1;i++){
+                if(i>0 && nums[i] == nums[i-1])continue;
             for(int j = i+1;j<n;j++){
-                int k = 0, l = n-1;
-                while(k<=l){
+                if(j != i+1 && nums[j] == nums[j-1])continue;
+                int k = j+1, l = n-1;
+                while(k<l){
                     long long int sum = 0;
                     sum = 0LL +nums[i] + nums[j] + nums[k] + nums[l] ;
                     if(target == sum ){
-                        if(i!=j && i != k && i!= l && j != k && j!=l && k!=l){
+                       
                             vector<int>v{nums[i],nums[j],nums[k],nums[l]};
-                            sort(v.begin(),v.end());
-                            if(mp[{v[0],{v[1],{v[2],v[3]}}}] == 0){
-                                mp[{v[0],{v[1],{v[2],v[3]}}}] ++;
-                                vp.push_back(v);
-                            }
-                            
-                        }
+                          
+                            vp.push_back(v);  
+                        
                         k++;
                         l--;
+                        while( k < l && nums[k] == nums[k-1])k++;
+                        while(k < l && nums[l] == nums[l+1])l--;
 
                     }else if(sum > target ){
                         l--;
