bb4cc7c1632c21ff0e5f14e5d6ddbbcb1849b131
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-09 08:44:55 +0600

    Time: 12 ms (83.75%), Space: 13.1 MB (84.64%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        
         int minimumAnswer  = target,minimum = INT_MAX;
-        sort(nums.begin(),nums.end());
-        for(int i = 0 ;i < nums.size();i++){
-            cout << nums[i]<<" ";
-        }
-        cout <<endl;
-        for(int i = 0 ; i < nums.size()-2; i++){
-            int j = i + 1, k = nums.size() - 1;
+        
+        sort(nums.begin(),nums.end()); int n = nums.size();
+       
+        for(int i = 0 ; i < n - 2; i++){
+            
+            int j = i + 1, k = n - 1;
 
             while(j < k){
+
                 int sum = nums[i] + nums[j] + nums[k];
+                
+                int closest   = abs(target-sum);
+                
+                if(closest < minimum){
+                    minimum = closest ; 
+                    minimumAnswer = sum;
+                }
+                
                 if(sum <= target){
-                    int x = abs(target-sum);
-                    if(x<minimum){
-                        minimum = x;
-                        minimumAnswer = sum;
-                    }
                     j++;
                 }else{
-                    int x = abs(target-sum);
-                    if(x<minimum){
-                        minimum = x;
-                        minimumAnswer = sum;
-                    }
                     k--;
                 }
             }
