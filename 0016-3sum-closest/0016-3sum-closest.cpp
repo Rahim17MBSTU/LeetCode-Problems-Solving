6f7836a6ce9b9e5e385193b55dcdd7a8302af97d
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-09 08:56:25 +0600

    Time: 12 ms (83.75%), Space: 13.1 MB (84.64%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -4,39 +4,30 @@ public:
         
         int minimumAnswer  = target,minimum = INT_MAX;
         
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end()); int n = nums.size();
+       
+        for(int i = 0 ; i < n - 2; i++){
+            
+            int j = i + 1, k = n - 1;
 
-        for(int i=0;i<nums.size() - 1;i++){
-
-            if(i>0 && nums[i] == nums[i-1])continue;
-
-            int j = i+1;
-
-            int k = nums.size() - 1;
-
-            while(j<k){
+            while(j < k){
 
                 int sum = nums[i] + nums[j] + nums[k];
+                
                 int closest   = abs(target-sum);
                 
                 if(closest < minimum){
                     minimum = closest ; 
                     minimumAnswer = sum;
                 }
-                if(sum < target){
+                
+                if(sum <= target){
                     j++;
-                }else if(sum > target){
-                    k--;
                 }else{
-                    j++;
                     k--;
-                    while(j<k && nums[j] == nums[j-1])j++;
-                    while(j<k && nums[k] == nums[k+1]) k--;
-
                 }
             }
-        }   
-       
+        }
         return minimumAnswer;
     }
 };
