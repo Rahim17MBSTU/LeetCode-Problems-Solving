8b4612ce498b539456f69c849e16f9766a33284e
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-13 00:17:58 +0600

    Time: 302 ms (53.83%), Space: 214.7 MB (14.41%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        vector<int>vis(1e6+10,0);
-        for(int i = 0;i < intervals.size();i++){
+        
+        int mx = INT_MIN;
+        int mn = INT_MAX;
+        for(int i=0;i<intervals.size();i++){
             int x = intervals[i][0];
+            
             int y = intervals[i][1];
+            mx = max(mx,y);
+            mn = min(mn,x);
+        }
+        vector<int>vis(mx+10,0);
+        for(int i = 0;i < intervals.size();i++){
+            
+            int x = intervals[i][0];
+            
+            int y = intervals[i][1];
+            
             vis[x] += 1;
+            
             vis[y+1] -= 1;
 
+        
         }
-        int minimumNumberGroup = 0 ;
-        for(int i=1;i<=1e6;i++){
-            vis[i] += vis[i-1];
-            minimumNumberGroup = max(minimumNumberGroup,vis[i]);
+        int minimumNumberGroup = INT_MIN;
+        for(int i=mn;i<=mx;i++){
+
+            vis[i] +=vis[i-1];
+            minimumNumberGroup = max(minimumNumberGroup , vis[i]);
         }
+        
+        
+        
         return minimumNumberGroup;
 
         
