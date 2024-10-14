c5f09309fc93ca425f76fbed3f4f26eaf86dbc71
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-14 08:53:47 +0600

    Time: 190 ms (69.83%), Space: 76 MB (88.03%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
 
-        priority_queue<int>pq(nums.begin(),nums.end());
+        priority_queue<int>pq(nums.begin(),nums.end()); // heapify use time complexity O(n)
         long long answer = 0;
         
         while(k>=1 && !pq.empty()){
 
-            int value = pq.top();
-            pq.pop();
+            int value = pq.top(); // O(1)
+            
+            pq.pop(); //O(log(n))
             answer += value;
             value = ceil((double)value/3.0);
-            pq.push(value);
+            pq.push(value); // O(log(n))
             k--;
 
+
         }
-        
+
         return answer;
     }
 };
