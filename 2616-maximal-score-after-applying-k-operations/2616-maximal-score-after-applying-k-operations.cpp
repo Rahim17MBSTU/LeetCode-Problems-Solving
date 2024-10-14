5a5e6dbf96bf3d163dd6c3e32dc269e2895ffcbd
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-14 09:02:17 +0600

    Time: 185 ms (82.04%), Space: 76.1 MB (79.55%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -5,15 +5,15 @@ public:
         priority_queue<int>pq(nums.begin(),nums.end()); // heapify use time complexity O(n)
         long long answer = 0;
         
-        while(k>=1 && !pq.empty()){
+        while(k--){
 
             int value = pq.top(); // O(1)
             
-            pq.pop(); //O(log(n))
+            pq.pop(); // O(log(n))
             answer += value;
-            value = ceil((double)value/3.0);
+            value = ceil(value/3.0);
             pq.push(value); // O(log(n))
-            k--;
+           
 
 
         }
