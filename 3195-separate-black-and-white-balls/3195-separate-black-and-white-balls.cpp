6b6dda56d9b137fe4e83710bf6e54b489e90f088
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-15 07:36:28 +0600

    Time: 37 ms (25.68%), Space: 15.5 MB (76.42%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -3,9 +3,9 @@ public:
     long long minimumSteps(string s) {
         int cnt = 0;
         long long  answer = 0;
-        for(int i = 0 ; i < s.size();i++){
+        for(int i = 0;i < s.size();i++){
             if(s[i] == '0'){
-                answer += (i-cnt);cnt++;
+                answer += (i-cnt++);
             }
         }
         return answer;
