e2356bf5ec3b3142cd06f98ba5864f4f0210904e
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-22 17:23:45 +0600

    Time: 0 ms (100%), Space: 8.5 MB (84.91%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-       unordered_map<char,int>mp;
+       vector<int>mp(130,0);
        for(int i = 0;i<t.size();i++){
         if(mp[t[i]] == 0){
-            mp[t[i]] = s[i] - 96;
+            mp[t[i]] = s[i];
         }else{
-            if( mp[t[i]] != s[i]-96) {
+            if( mp[t[i]] != s[i]) {
                 return false;
             }else{
                 continue;
             }
          }
-       } unordered_map<char,int>mp1;
+       } 
+       vector<int>mp1(130,0);
        for(int i = 0;i<t.size();i++){
         if(mp1[s[i]] == 0){
-            mp1[s[i]] = t[i] - 96;
+            mp1[s[i]] = t[i] ;
         }else{
-            if( mp1[s[i]] != t[i]-96) {
+            if( mp1[s[i]] != t[i]) {
                 return false;
             }else{
                 continue;
