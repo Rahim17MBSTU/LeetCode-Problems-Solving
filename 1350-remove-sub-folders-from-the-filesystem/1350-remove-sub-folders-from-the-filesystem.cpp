bf6155f3e08d924cdce3e31422a07590779470dc
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-25 09:59:58 +0600

    Time: 47 ms (95.6%), Space: 50 MB (70.6%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -3,18 +3,13 @@ public:
     vector<string> removeSubfolders(vector<string>& folder) {
         sort(folder.begin(),folder.end());
         vector<string>v;
-        bool ok = 0;
         string res = "";
-        for(auto u:folder){
-            if(ok == 0){
-                ok = 1;
-                res = u;//v.push_back(u);
-            }else{
-                string s = u;
+        for(int i = 0 ;i < folder.size();i++){
+            string u = folder[i];
+            if(i){
+                
                 if(res.size() == u.size()){
-                    if(res == u)
-                        ;
-                    else{
+                    if(res != u){
                         
                         v.push_back(res);res = u;
 
@@ -22,24 +17,26 @@ public:
                     continue;
                 }
                 int i = 0;
-                for(;i<s.size();i++){
-                    if(i<res.size() && s[i] != res[i]){
+                for(;i<u.size();i++){
+                    if(i<res.size() && u[i] != res[i]){
                         v.push_back(res);res= u;break;
                     }
-                    if(i<res.size() && s[i] == res[i])continue;
-                    if(i>=res.size() && s[i] == '/') break;
-                    if(i>=res.size() && s[i] >='a' && s[i] <='z'){
+                    if(i<res.size() && u[i] == res[i])continue;
+                    if(i>=res.size() && u[i] == '/') break;
+                    if(i>=res.size() && u[i] >='a' && u[i] <='z'){
                         v.push_back(res);
                         res = u;
                         break;
                     }
                 }
 
+            }else{
+                res= u;
             }
             
         }
 
-        cout<<"res= "<<res<<endl;
+        //cout<<"res= "<<res<<endl;
         if(v.size()>=1){
             if(v.back() != res)v.push_back(res);
         }else{
