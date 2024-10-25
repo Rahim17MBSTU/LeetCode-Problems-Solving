3673b052647cf89dfea621bbae8837590399e907
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-25 10:19:05 +0600

    Time: 49 ms (94.78%), Space: 49.8 MB (71.15%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -3,45 +3,17 @@ public:
     vector<string> removeSubfolders(vector<string>& folder) {
         sort(folder.begin(),folder.end());
         vector<string>v;
-        string res = "";
-        for(int i = 0 ;i < folder.size();i++){
-            string u = folder[i];
-            if(i){
-                
-                if(res.size() == u.size()){
-                    if(res != u){
-                        
-                        v.push_back(res);res = u;
-
-                    }
-                    continue;
-                }
-                int i = 0;
-                for(;i<u.size();i++){
-                    if(i<res.size() && u[i] != res[i]){
-                        v.push_back(res);res= u;break;
-                    }
-                    if(i<res.size() && u[i] == res[i])continue;
-                    if(i>=res.size() && u[i] == '/') break;
-                    if(i>=res.size() && u[i] >='a' && u[i] <='z'){
-                        v.push_back(res);
-                        res = u;
-                        break;
-                    }
-                }
-
-            }else{
-                res= u;
-            }
+        string prev = "";
+        for(auto u:folder){
+            if(prev.empty() || u.compare(0,prev.size(),prev) != 0 || u[prev.size()]!='/'){
+                v.push_back(u);
+                prev = u;
+            }            
             
         }
 
-        //cout<<"res= "<<res<<endl;
-        if(v.size()>=1){
-            if(v.back() != res)v.push_back(res);
-        }else{
-            v.push_back(res);
-        }
+      
+       
         return v;
     }
 };
