b2e319db168dcbf2e7d1798b0042f9a5b86d6dc9
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-10-28 23:40:51 +0600

    Time: 15 ms (95.56%), Space: 126.9 MB (15.44%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -4,13 +4,14 @@ public:
     int longestSquareStreak(vector<int>& v) {
         int n = v.size();
         vector<int>mp(1e5+10,0);
-        set<int>s;
-        for(int i = 0 ; i < n; i++){
-            mp[v[i]]++;
-            s.insert(v[i]);
-        }
+        for(auto u:v)mp[u]++;
+        // set<int>s;
+        // for(int i = 0 ; i < n; i++){
+        //     mp[v[i]]++;
+        //     s.insert(v[i]);
+        // }
         int mx = 0,cnt = 0 ;
-        for(auto u:s){
+        for(auto u:v){
             long long  x = u;
             int cnt = 1;
             while(1LL*x*x <= 1e5){
