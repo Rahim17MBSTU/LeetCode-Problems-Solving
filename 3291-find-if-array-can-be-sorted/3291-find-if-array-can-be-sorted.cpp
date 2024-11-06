314e6f14def285826d0b41be09d82c9c46d14c35
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-11-06 14:23:43 +0600

    Time: 7 ms (24.46%), Space: 33 MB (15.69%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -4,42 +4,33 @@ public:
     bool canSortArray(vector<int>& nums) {
         vector<pair<int,int>>vp;
         if(is_sorted(nums.begin(),nums.end())){
-            cout<<"aise"<<endl;
             return true;
         }
         for(int i = 0 ;i < nums.size();i++){
             int bit = __builtin_popcount(nums[i]);
             vp.push_back({bit,nums[i]});
         }
-        // for(auto u:vp){
-        //     cout<<u.first<< " "<<u.second<<endl;
-        // }
         vector<int>res,res1;
-
         int cnt = vp[0].first;
-        int mx  = vp[0].second;
-        int mx1 = vp[0].second;
-
+        int mn  = vp[0].second;
+        int mx = vp[0].second;
 
         for(int i = 0 ; i < vp.size();i++){
             if(cnt == vp[i].first){
-                mx = min(mx,vp[i].second);
-                mx1 =max(mx1,vp[i].second);
+                mn = min(mn,vp[i].second);
+                mx =max(mx,vp[i].second);
             }else{
+                res.push_back(mn);
                 res.push_back(mx);
-                res.push_back(mx1);
-                mx = vp[i].second;
-                mx1 = mx;
+                mn = vp[i].second;
+                mx = mn;
                 cnt= vp[i].first;
             }
         }
         
-            res.push_back(mx);
-             for(auto u:res)cout<<u<<" ";
-        cout<<endl;
-        //}
-       if(is_sorted(res.begin(),res.end()) == true && is_sorted(res1.begin(),res1.end())){
-        // cout<<"aise1"<<endl;
+        res.push_back(mn);
+        res1.push_back(mx);
+        if(is_sorted(res.begin(),res.end()) && is_sorted(res1.begin(),res1.end())){
             return true;
         }
         else 
