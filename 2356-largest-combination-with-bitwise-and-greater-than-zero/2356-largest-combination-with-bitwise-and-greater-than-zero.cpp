00e29db22e3805f9dbd2cdf970346065cc9996f0
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-11-07 10:57:33 +0600

    Time: 51 ms (10.45%), Space: 60.1 MB (88.86%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -5,19 +5,16 @@ public:
         int size = candidates.size();
 
         vector<int>res(25,0);
-        bitset<25>b[size];
-
-        for(int i=0;i<size;i++){
-            b[i] = candidates[i];
-        }
+        
         int mxLenght = 0;
 
-        for(int i = 0 ;i < size ;i++){
+        for(int i = 24 ;i >= 0 ;i--){
             
-            for(int j = 24 ; j >= 0;j--){
-                res[j] += b[i][j];
-                mxLenght = max(mxLenght , res[j]);
-            }
+            for(auto u: candidates){
+                if((u & (1 << i)) != 0)res[i]++;
+              
+            } 
+            mxLenght = max(mxLenght , res[i]);
         }
         return mxLenght;
     }
