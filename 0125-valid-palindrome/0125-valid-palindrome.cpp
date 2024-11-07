35e473b21d7e01fb9402e0351adc551dd719f55d
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-11-07 17:17:42 +0600

    Time: 1 ms (44.79%), Space: 9.4 MB (27.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i = 0 , j = s.size()-1;
-        
+        string temp = "";
+        for(int i = 0 ; i < s.size() ; i++){
+            s[i] = tolower(s[i]);
+            if(s[i] >='a' && s[i] <='z')temp +=s[i];
+            if(s[i] >='0' && s[i] <='9')temp +=s[i];
+        }
+        int  i = 0 , j = temp.size()-1;
         while(i<=j){
-             s[i] = tolower(s[i]);
-             s[j] = tolower(s[j]);
-            if(s[i] >= 'a'  && s[i] <='z' && s[j] >='a' && s[j]<='z'){
-                if(s[i] != s[j])return false;
-                i++,j--;
-                continue;
-            }
-            if(s[i] >= '0'  && s[i] <='9' && s[j] >='0' && s[j]<='9'){
-                if(s[i] != s[j])return false;
-                i++,j--;
-                continue;
-            }if(s[i] >= '0'  && s[i] <='9' && s[j] >= 'a' && s[j] <='z'){
+            if(temp [i] != temp[j]){
                 return false;
             }
-            if(s[i] >= 'a'  && s[i] <='z' && s[j] >= '0' && s[j] <='9'){
-                return false;
-            }
-            if(s[i] >= 'a'  && s[i] <='z'){
-                j--;continue;
-            }if(s[j] >= '0' && s[j] <='9'){
-                i++;continue;
-            }if(s[i] >= '0'  && s[i] <='9'){
-                j--;continue;
-            }if(s[j] >= 'a' && s[j] <='z'){
-                i++;continue;
-            }
-            else 
-            i++,j--;
+            i++;
+            j--;
         }
         return true;
     }
