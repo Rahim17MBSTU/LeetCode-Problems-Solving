212a6bd69fec979c32299b21913d98d61568f88a
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-11-15 12:11:33 +0600

    Time: 0 ms (100%), Space: 69.6 MB (25.81%) - LeetHub

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,45 +1,20 @@
 class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
-        
-       vector<int>v(arr.size(),0);
-       int cnt = 1,n = arr.size(),ans=1;
-       v[n-1] = 1;
-       for(int i = arr.size()-2;i>=0;i--){
-         if(arr[i] <= arr[i+1]){
-            v[i] = 1;
-            cnt++;
-         }else{
-            break;
-         }
-       }
-       ans = n-cnt;
-       //cout <<" === "<< ans <<endl;
-       for(auto u:v)cout<<u<<" ";
-       cout<<endl;
-       auto f = [&](int mid,int value){
-           if(arr[mid] >= value && v[mid] == 1) return true;
-           else return false;
-       };
-       for(int i = 0 ; i<n-1;i++){
-         int value = arr[i],indx = n;
-         int low = i+1;
-         int high = n-1;
-         
-         while(low<=high){
-            int mid = (low + high)>> 1;
-            if(f(mid,arr[i])){
-                indx = mid;
-                high = mid - 1;
-            }else{
-                low = mid + 1;
+        int n = arr.size();
+        int low = 0 , high = n-1;
+        while(high>0 && arr[high]>=arr[high-1]){
+            high--;
+        }
+        int ans = high;
+        while(low<high){
+            while(high < n && arr[low] > arr[high]){
+                high++;
             }
-         }
-         //cout<<i<< " "<<indx-1<<endl;
-         ans = min(ans,indx - 1 - i);
-         if(arr[i] > arr[i+1])break;
-       }
-     
+            ans = min(ans,high - low - 1);
+            low ++;
+            if(low>0 && arr[low]<arr[low-1])break;
+        }
         return ans;
     }
 };
