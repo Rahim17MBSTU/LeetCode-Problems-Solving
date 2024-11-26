c1218c24a75ba9a8640ccecc284745ce471a6b52
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-11-26 12:19:05 +0600

    Time: 34 ms (51.4%), Space: 104 MB (44.16%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.cpp
+++ 3189-find-champion-ii/3189-find-champion-ii.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
      
-        vector<int>Ingoing(n);
-        int ans = 0;
-        for(vector<int>v :edges){
-            int x = v[0];
-            int y = v[1];
-            Ingoing[y]++;
-            
+        vector<int>InDegree(n,0);
+        int ans = -1 ,cnt = 0;
+        for(vector<int>e :edges){
+            int u = e[0];
+            int v = e[1];
+             // u --> v
+            InDegree[v]++;
         
         }
-        int cnt = 0;
-        for(int i = 0 ; i < n;i++) if(Ingoing[i] == 0) {
-            ans = i;
-            cnt++;
-           if(cnt>=2 )return -1;
+        
+        for(int i = 0 ; i < n;i++) 
+            if(InDegree[i] == 0) {
+                ans = i;
+                cnt++;
+            if(cnt>=2 )return -1;
         } 
         return ans;
     }  
