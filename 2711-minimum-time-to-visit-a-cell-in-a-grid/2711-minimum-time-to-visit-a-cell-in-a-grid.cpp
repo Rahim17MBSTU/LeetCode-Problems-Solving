5b19e54655ae49dd409cd5a578edbb84ded0f9c1
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-11-29 15:56:49 +0600

    Time: 127 ms (97.06%), Space: 51.3 MB (90.57%) - LeetHub

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -1,53 +1,46 @@
 class Solution {
 public:
-    int dx[4] = {-1, 1, 0, 0};
-    int dy[4] = {0, 0, -1, 1};
-    
+    #define P pair<int,pair<int,int>>
+    int dx[4] = {1,-1,0,0};
+    int dy[4] = {0,0,1,-1};
     int minimumTime(vector<vector<int>>& grid) {
-        if (grid[0][1] > 1 && grid[1][0] > 1) return -1;
-        
+        if(grid[0][1] > 1 && grid[1][0] > 1)return -1;
         int n = grid.size();
         int m = grid[0].size();
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        //vector<vector<int>> res(n,vector<int>(m,0));
-        
-        pq.push({0, {0, 0}});
-        
-        while (!pq.empty()) {
-            int dis = pq.top().first;
+        vector<vector<bool>>vis(n,vector<bool>(m,0));
+        priority_queue<P,vector<P> ,greater<P>>pq;
+        pq.push({0,{0,0}});
+        vis[0][0] = 1;
+        int answer = -1;
+        while(!pq.empty()){
+            int time = pq.top().first;
             int row = pq.top().second.first;
             int col = pq.top().second.second;
             pq.pop();
-            
-            
-             if (row == n - 1 && col == m - 1) {
-               
-                return dis;
-            }
-            if (vis[row][col]) continue;
-            vis[row][col] = true;
-            
-         
-            
-            for (int i = 0; i < 4; i++) {
-                int x = row + dx[i];
-                int y = col + dy[i];
-                
-                if (x >= 0 && y >= 0 && x < n && y < m && !vis[x][y]) {
-                    if (dis + 1 >= grid[x][y]) {
-                        pq.push({dis + 1, {x, y}});
-                        //res[x][y] = dis+1;
-                    } else {
-                        int wait_time = grid[x][y] - dis;
-                        int next_time = (wait_time % 2 == 0) ? grid[x][y]+1 : grid[x][y];
-                        pq.push({next_time, {x, y}});
-                         //res[x][y] = next_time;
+        
+            if(row == n-1 && col == m-1){
+                return time;
+            } 
+           
+            for(int i = 0 ;i < 4;i++){
+                int x = dx[i] + row;
+                int y = dy[i] + col;
+                if(x >= 0 && y >= 0 && x < n && y < m && !vis[x][y] ){
+                    if(time + 1 >= grid[x][y]){
+                        pq.push({time+1,{x,y}});
+                    }else{
+                        if((grid[x][y] - time ) % 2 == 0){
+                            pq.push({grid[x][y] + 1,{x,y}});
+                        }else{
+                            pq.push({grid[x][y] ,{x,y}});
+                        }
                     }
+                    vis[x][y] = 1;
+                    
                 }
             }
         }
-        
         return -1;
+        
     }
 };
