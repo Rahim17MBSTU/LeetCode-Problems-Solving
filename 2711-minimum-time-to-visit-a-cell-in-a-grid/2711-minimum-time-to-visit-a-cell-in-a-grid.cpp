45d52e3bbef27876950c849d4b8dd47832a07517
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-11-29 15:20:16 +0600

    Time: 1655 ms (5.15%), Space: 67.8 MB (46.63%) - LeetHub

---
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+    
+    int minimumTime(vector<vector<int>>& grid) {
+        if (grid[0][1] > 1 && grid[1][0] > 1) return -1;
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        vector<vector<int>> res(n,vector<int>(m,0));
+        
+        pq.push({0, {0, 0}});
+        
+        while (!pq.empty()) {
+            int dis = pq.top().first;
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
+            pq.pop();
+            
+            // If this cell has already been visited, skip it
+             if (row == n - 1 && col == m - 1) {
+                for(int i = 0 ;i <n;i++){
+                    for(int j = 0 ;j <m;j++){
+                        cout << res[i][j]<<" ";
+                    }
+                    cout<<endl;
+                }
+                return res[row][col];
+            }
+            if (vis[row][col]) continue;
+            vis[row][col] = true;
+            
+         
+            
+            for (int i = 0; i < 4; i++) {
+                int x = row + dx[i];
+                int y = col + dy[i];
+                
+                if (x >= 0 && y >= 0 && x < n && y < m && !vis[x][y]) {
+                    if (dis + 1 >= grid[x][y]) {
+                        pq.push({dis + 1, {x, y}});
+                        res[x][y] = dis+1;
+                    } else {
+                        int wait_time = grid[x][y] - dis;
+                        int next_time = (wait_time % 2 == 0) ? grid[x][y]+1 : grid[x][y];
+                        pq.push({next_time, {x, y}});
+                         res[x][y] = next_time;
+                    }
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
