2f6dcc415201032def587053c52dba71fbc18e28
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-12-10 22:14:20 +0600

    Time: 0 ms (100%), Space: 11 MB (75.18%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -2,41 +2,44 @@ class Solution {
 public:
     int maximumLength(string s) {
         int n = s.size();
-        int  low = 0, ans = -1 , high = n-1;
-        auto f = [&](int mid){
-            vector<int>fre(29,0);
-            map<string,int>mp;
-            string t = "";
-            for(int i = 0 ;i <= mid;i++){
-                
-                t +=s[i];fre[s[i] - 96]++;  
-                if(fre[s[i]-96] == mid + 1) {
-                    mp[t]++;
-                } 
+        int low = 0 , high = n-1;
+        
+        int ans = -1;
+        auto check = [&](int mid,vector<int>&fre){
+            for(int i=1;i<=26;i++){
+                if(fre[i] == mid+1) return true;
             }
-            for(int i = 0 , j = mid + 1; j < n ;j++,i++){
-                t += s[j];
-                reverse(t.begin(),t.end());
-                t.pop_back();
-                reverse(t.begin(),t.end());
+            return false;
+        };
+        auto f = [&] (int mid){
+            vector<int>fre(28,0);
+            map<pair<char,int>,int>mp;
+            for(int i = 0 ; i <= mid;i++){
+                fre[s[i] - 96]++;
+            }
+            char ch = s[0];
+            if(check(mid,fre)){
+                mp[{ch,mid}]++;
+            }
+            for(int i = 0, j = mid+ 1;j<n;j++,i++){
+                
                 fre[s[i]-96]--;
                 fre[s[j]-96]++;
-                for(int k=1;k<=26;k++){
-                    if(fre[k] == mid+1){
-                        mp[t]++;
-                        if(mp[t]>=3){
-                            return true;
-                        }
+                ch  = s[j];
+                if(check(mid,fre)){
+                    mp[{ch,mid}]++;
+                    if(mp[{ch,mid}]>=3){
+                        return true;
                     }
-                } 
+                }
             }
             return false;
         };
         while(low <= high){
             int mid = (low + high) >> 1;
             if(f(mid)){
-                ans = mid+1;
                 low = mid + 1;
+                ans = mid + 1;
             }else{
                 high = mid - 1;
             }
