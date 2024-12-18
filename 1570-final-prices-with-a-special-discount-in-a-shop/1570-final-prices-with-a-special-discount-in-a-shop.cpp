69f521abb1b8223a643883c4d1ffa1574dcade0d
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-12-18 09:57:03 +0600

    Time: 0 ms (100%), Space: 14.1 MB (17.75%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
         stack<pair<int,int>>st;
-        for(int i = 0 ; i < prices.size();i++){
-            if(!st.empty()){
-                if(st.top().first>= prices[i]){
-                    while(!st.empty()&&st.top().first>= prices[i]){
-                        int indx = st.top().second;
-                        st.pop();
-                        prices[indx] -= prices[i];
-                    }
-                    st.push({prices[i],i});
-                }else{
-                    st.push({prices[i],i});
+        for(int i = 0 ; i < prices.size();i++){ // Monotonic Stack apply to solve this problem 
+            if(!st.empty() && st.top().first >= prices[i]){
+                
+                while(!st.empty()&&st.top().first>= prices[i]){
+                    int indx = st.top().second;
+                    st.pop();
+                    prices[indx] -= prices[i];
                 }
+                st.push({prices[i],i});
+                
             }else{
                st.push({prices[i],i});
             }
