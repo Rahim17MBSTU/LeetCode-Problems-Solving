33eab00ecef3af18ec94d37a6b17913f7d6334fd
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2024-12-27 11:23:48 +0600

    Time: 0 ms (100%), Space: 47.3 MB (24.68%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -3,19 +3,18 @@ public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int n = values.size();
         vector<int>maxSum(n-1);
-        vector<int>minSum(n-1);
+      
         for(int i = 0 ;i<n-1;i++){
             maxSum[i] = i + values[i];
         }
         int mn = INT_MAX;
+        int answer = INT_MIN;
         for(int i= n-2;i>=0;i--){
             mn = min(mn,(i+1)-values[i+1]);
-            minSum[i] = mn;
+            answer = max(answer, maxSum[i] - mn);
         }
-        int mx = INT_MIN;
-        for(int i = 0 ;i<n-1;i++){
-            mx = max(mx,maxSum[i] - minSum[i]);
-        }
-        return mx;
+        
+        
+        return answer;
     }
 };
