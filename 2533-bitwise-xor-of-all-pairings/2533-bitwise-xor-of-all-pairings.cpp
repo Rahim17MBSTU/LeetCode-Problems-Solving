5e699fd2275d9669656e95f6aebd69f1c27e71c1
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2025-01-16 12:04:24 +0600

    Time: 0 ms (100%), Space: 64.4 MB (36.18%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -3,15 +3,13 @@ public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        int xorValue = 0;
-        for(auto u:nums2) xorValue ^= u;
+        int xorValue1 = 0, xorValue2 = 0;
+        for(auto u:nums1) xorValue1 ^= u;
+        for(auto u:nums2) xorValue2 ^= u;
         if(n % 2 == 0 && m % 2 == 0) return 0;
-        if(n % 2 == 1 && m % 2 == 0) return xorValue;
-        int ans = 0;
-        for(auto u:nums1){
-            int x = xorValue ^ u;
-            ans ^= x;
-        }
-        return ans;
+        if(n % 2 == 1 && m % 2 == 0) return xorValue2;
+        if(n % 2 == 0 && m % 2 == 1) return xorValue1;
+        else  return (xorValue1 ^ xorValue2);
+          
     }
 };
