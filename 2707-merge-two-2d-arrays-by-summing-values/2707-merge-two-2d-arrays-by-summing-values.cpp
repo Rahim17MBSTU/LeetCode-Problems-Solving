5f078c312c0d9d8b0599ac5010cb834b0b09af81
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2025-03-02 11:44:48 +0600

    Time: 0 ms (100%), Space: 14.7 MB (82.78%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        map<int,int>mp;
-        for(auto u:nums1) mp[u[0]]+=u[1];
-        for(auto u:nums2) mp[u[0]]+=u[1];
-        vector<vector<int>> result;
-        int cnt = 0;
-        for(auto u:mp){
-            result.push_back({u.first,u.second});
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<vector<int>>result;
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0 , j = 0;
+        while(1){
+            if(i<n && j < m && nums1[i][0] < nums2[j][0]){
+                result.push_back({ nums1[i][0],nums1[i][1]});
+                i++;
+            }
+             if(i<n && j <m && nums1[i][0] > nums2[j][0]){
+                result.push_back({nums2[j][0],nums2[j][1]});
+                j++;
+            }
+            if(i<n && j < m && nums1[i][0] == nums2[j][0]){
+                result.push_back({nums2[j][0], nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            }
+             if(i>=n && j < m ){
+                result.push_back({nums2[j][0],nums2[j][1]});
+                j++;
+            }
+             if(i<n && j >= m ){
+                result.push_back({nums1[i][0],nums1[i][1]});
+                i++;
+            }
+
+            if(i >= n && j >= m)break;
         }
         return result;
+        
     }
 };
