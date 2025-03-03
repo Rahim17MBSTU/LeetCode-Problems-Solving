c66cfc7c4efb75b8ef6fd5495e3717629a26116e
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2025-03-03 12:56:06 +0600

    Time: 3 ms (100%), Space: 87.9 MB (83.72%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cs
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cs
@@ -1,18 +1,20 @@
 public class Solution {
     public int[] PivotArray(int[] nums, int pivot) {
-        List<int> Less = new List<int>();
-        List<int> Equal = new List<int>();
-        List<int> Greater = new List<int>();
-        List<int> Result = new List<int>();
-        foreach(var u in nums){
-            if(pivot < u) Greater.Add(u);
-            else if(pivot > u) Less.Add(u);
-            else Equal.Add(u);
+        int n = nums.Length;
+        int[] Result = new int[n];
+        
+        int left = 0 , right = n - 1;
+        
+        for(int i = 0 , j = n - 1; i < n ; i++,j--){
+            if(nums[i] < pivot){
+                Result[left++] = nums[i];
+            }if(nums[j] > pivot) {
+                Result[right--] = nums[j];
+            }
         }
-
-        Result.InsertRange(Result.Count,Less);
-        Result.InsertRange(Result.Count,Equal);
-        Result.InsertRange(Result.Count,Greater);
-        return Result.ToArray();
+        while(left <= right){
+            Result[left++] = pivot;
+        }
+        return Result;
     }
 }
