e33fc0b901bf72d2a64852202f898abaaf23eed7
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2025-03-11 15:34:53 +0600

    Time: 8 ms (61.55%), Space: 11.1 MB (35.03%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
+    bool check(int &a, int &b , int &c){
+        if(a>=1 && b>=1 && c>=1) return 1;
+        return 0;
+    }
+    void Increment(int &a,int &b,int &c,char &ch, bool ok){
+        if(ok){
+            if(ch == 'a')a++;
+            if(ch == 'b')b++;
+            if(ch == 'c')c++;
+        }else{
+            if(ch == 'a')a--;
+            if(ch == 'b')b--;
+            if(ch == 'c')c--;
+        }
+    }
     int numberOfSubstrings(string s) {
         int n = s.size(), ans = 0 ;
         int i = 0 , j = 0 ; 
-        map<char,int>mp;
+        int a = 0 , b = 0 , c = 0;
         while(j<n){
-            while(mp.size() < 3 && j <n){
-                mp[s[j]]++;      
+            while(!check(a,b,c) && j <n){  
+                Increment(a,b,c,s[j],true);    
                 j++;
             }
-            while(mp.size() == 3 && i<j){
-                mp[s[i]]--;
-                if(mp[s[i]] == 0)mp.erase(s[i]);
+            while(check(a,b,c) && i<j){
+                Increment(a,b,c,s[i],false);
                 i++;
                 ans += n-j+1;
             }
