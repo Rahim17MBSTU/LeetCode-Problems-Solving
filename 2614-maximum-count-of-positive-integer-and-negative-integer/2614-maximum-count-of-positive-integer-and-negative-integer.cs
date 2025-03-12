a8512f47d53fc31f16c46a5bd513b77efed9c917
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2025-03-12 12:06:25 +0600

    Time: 1 ms (56.03%), Space: 45.7 MB (93.62%) - LeetHub

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int MaximumCount(int[] nums) {
+        int p = 0 , n = 0;
+        for(int i = 0 ; i < nums.Length; i++){
+            if(nums[i] < 0)n++;
+            if(nums[i] >0)p++;
+        }
+        return Math.Max(p,n);
+    }
+}
