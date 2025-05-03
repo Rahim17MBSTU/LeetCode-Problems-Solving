1e4c2a99a8b168fe0fa1d93e0d8269ad1241f26a
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2025-05-03 10:29:17 +0600

    Time: 7 ms (34.91%), Space: 115.4 MB (48.71%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        map<int,int>mp1,mp2;
-        int mx1 = 0, mx2 = 0, ans = -1, mx_cnt1 = 0 , mx_cnt2 = 0;
-        for(auto u:tops){
-            mp1[u]++;
-            if(mx_cnt1 <= mp1[u]){
-                mx_cnt1 = mp1[u];
-                mx1 = u;
+        int ans = 1e9;
+        for(int i = 1; i<=6; i++){
+            int cnt = 0;
+            bool ok = 0;
+            //cout<<"i = "<<i<<" -> ";
+            for(int j = 0;j<tops.size();j++){
+                if(tops[j] == i)continue;
+                else if(bottoms[j] == i)cnt++;
+                else{
+                    ok = 1;
+                    break;
+                }
             }
-           
-        }
-        for(auto u:bottoms){
-            mp2[u]++;
-            if(mx_cnt2 <= mp2[u]){
-                mx_cnt2 = mp2[u];
-                mx2 = u;
+            if(ok == 0){
+                ans = min(ans,cnt);
+            }
+            //cout<<ok<<" ";
+            cnt = 0;
+            ok = 0;
+            for(int j = 0;j<tops.size();j++){
+                if(bottoms[j] == i)continue;
+                else if(tops[j] == i)cnt++;
+                else{
+                    ok = 1;
+                    break;
+                }
+            }
+            //cout<<ok<<'\n';
+            if(ok == 0){
+                ans = min(ans,cnt);
             }
         }
-        bool ok1 = 0, ok2 = 0;
-        int cnt1 = 0 , cnt2 = 0;
-        for(int i=0;i<tops.size();i++){
-            if(tops[i] == mx1)continue;
-            else if(bottoms[i] == mx1)cnt1++;
-            else ok1 = 1;
-        }
-        for(int i=0;i<tops.size();i++){
-            if(bottoms[i] == mx2)continue;
-            else if(tops[i] == mx2)cnt2++;
-            else ok2 = 1;
-        }
-        cout<<mx1<<" "<<mx2<<endl;
-        if(ok1 == 1 && ok2 == 1)return -1;
-        if(ok1 == 1 && ok2 == 0)return cnt2;
-        if(ok1 == 0 && ok2 == 1)return cnt1;
-        if(ok1 == 0 && ok2 == 0)return min(cnt1,cnt2);
-        return cnt1;
+        return ans == 1e9 ? -1 : ans;
     }
 };
