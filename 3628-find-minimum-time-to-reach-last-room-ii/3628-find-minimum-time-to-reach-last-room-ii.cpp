0e31e27bdc244051cdd05ae3507337546dc75bd3
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2025-05-08 12:10:25 +0600

    Time: 167 ms (98.95%), Space: 89.8 MB (98.95%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
@@ -3,39 +3,55 @@ public:
     int dx[4] = {0,1,0,-1};
     int dy[4] = {1,0,-1,0};
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int answer = 0;
-        int n = moveTime.size();
-        int m = moveTime[0].size();
-        vector<vector<bool>>vis(n,vector<bool>(m,0));
-        priority_queue<pair<int,pair<int,pair<int,int>>>, vector<pair<int,pair<int,pair<int,int>>>> , greater<pair<int,pair<int,pair<int,int>>>>> pq;
-        pq.push({0,{0,{0,2}}});
-        while(!pq.empty()){
-            int value = pq.top().first;
-            int x = pq.top().second.first;
-            int y = pq.top().second.second.first;
-            int inc = pq.top().second.second.second;
-            //cout<<value <<" "<<x <<" "<<y<<" "<<inc<<endl;
-            if(x == n-1 && y == m-1){
-                answer = value ;
+        int row = moveTime.size();
+        int col = moveTime[0].size();
+
+        int answer = 0 ; 
+        vector<vector<bool>> visited(row, vector<bool>(col,0));
+
+        priority_queue<pair<int,pair<int,pair<int,int>>> , vector<pair<int,pair<int,pair<int,int>>>> , greater<pair<int,pair<int,pair<int,int>>>>> Priority_queue;
+
+        Priority_queue.push({0,{0,{0,2}}});
+        visited[0][0] = 1;
+        
+        while(!Priority_queue.empty()){
+
+            int oldValue = Priority_queue.top().first;
+            int oldRow = Priority_queue.top().second.first;
+            int oldCol = Priority_queue.top().second.second.first;
+            int moveValue = Priority_queue.top().second.second.second;
+
+            Priority_queue.pop();
+           
+            if(oldRow == row - 1 && oldCol == col-1){
+                answer = oldValue;
                 break;
             }
-            pq.pop();
-            if(inc == 2) inc = 1;
-            else inc = 2;
+            moveValue = (moveValue == 2) ? 1 : 2 ; 
             for(int i = 0 ; i < 4; i++){
-                int X = x + dx[i];
-                int Y = y + dy[i];
-                if(X >= n || X < 0 || Y >= m || Y < 0 || vis[X][Y] ) continue;
-                vis[X][Y] = 1;
-                
-                if(value < moveTime[X][Y]){
-                     
-                     pq.push({moveTime[X][Y]+inc,{X,{Y,inc}}});
+
+                int newRow = oldRow + dx[i];
+                int newCol = oldCol + dy[i];
+
+                if(newRow < 0 || newCol < 0 || newRow >= row || newCol >= col || visited[newRow][newCol]) continue;
+                visited[newRow][newCol] = 1;
+
+                if(oldValue < moveTime[newRow][newCol]){
+
+                    int newValue = moveTime[newRow][newCol] + moveValue;
+                    Priority_queue.push({newValue,{newRow,{newCol,moveValue}}});
+
                 }else{
-                    pq.push({value+inc,{X,{Y,inc}}});
+
+                    int newValue = oldValue + moveValue;
+                    Priority_queue.push({newValue,{newRow,{newCol,moveValue}}});
+
                 }
             }
+    
         }
+
         return answer;
+
     }
 };
