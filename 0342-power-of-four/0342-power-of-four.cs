19014da302b03c4a1bdb9cbaff78ac5ff3af4b69
Abdur Rahim <80756631+Rahim17MBSTU@users.noreply.github.com>
2025-08-15 09:56:58 +0600

    Time: 0 ms (100%), Space: 30.6 MB (95.27%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cs
+++ 0342-power-of-four/0342-power-of-four.cs
@@ -1,17 +1,19 @@
 public class Solution {
     public bool IsPowerOfFour(int n) {
-        int low = 0, high = 15;
-        while(low<=high){
-            int mid =(int)(low + high) / 2;
-            long value = (long) Math.Pow(4,mid);
-            if(value == n){
-                return true;
-            }else if(value > n){
-                high = mid - 1;
-            }else{
-                low = mid + 1;
-            }
-        }
+        // here n must be positive integer 
+        // n&n-1 == 0 then this number is power of 2 
+        /*      8 = 1000
+                7 = 0111
+            -----------------
+                0 = 0000
+
+        */
+        // n & 0x55555555 means hexadecimal representation
+        // power of 4 mesna even postion any bit must be set bit
+        // ex: 16 = 10000 
+        // 10101010101010101010101 = means even postion now we (n&0x55555555) != 0 means power of 4
+        
+        if(n >= 1 && (n&(n-1)) == 0 && (n& 0x55555555) != 0)return true;
         return false;
     }
 }
